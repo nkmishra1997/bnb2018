33b2277c962c576a979a6d9df4772c2fae35bb8c
vegeta1509 <ayushmvi123@gmail.com>
2017-07-04 00:23:35 +0530

    Update customer.js

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,7 +1,6 @@
 // app/models/user.js
 // load the things we need
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
