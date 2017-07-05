f9b535543e009ff3bcef9141317c2d2222d14d95
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2017-07-05 22:02:21 +0530

    more routes

--- app/req_routes.js
+++ app/req_routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./models/customer.js');
-//const News = require('./models/news.js');
+const News = require('./models/news.js');
 //const Company = require('./models/company.js');
 
 const mongoose = require('mongoose');
@@ -11,7 +11,32 @@ mongoose.Promise = global.Promise;
 router.get('/customerdetail',(request,response)=>{
   User.findById(request.user._id).populate('stockHoldings.company').populate('stockShorted.company').populate('activity.company').exec((err,user)=>{
     if(err){console.log(err);}
-    else{response.json(user);}
+    else{response.json(user).pretty();}
   });
+});
+
+router.get('/newsdetail/:id',(request,response)=>{
+  News.findById(request.params.id,(err,news)=>{
+    if (err){console.log(err);}
+    else{response.json(news).pretty();}
+  });
+});
+
+router.get('/companydetail',(request,response)=>{
+  Company.findById(request.company._id,(err,company)=>{
+    if(err){console.log(err);}
+    else{response.json(comapany).pretty();}
+  });
+});
+
+router.get('/newslist',(request,response)=>{
+  News.find({}).then((news)=>{
+    response.json(news).pretty();
+    done();
+  });
+});
+
+router.get('/leaderboard',(request,response)=>{
+  response.send('Leaderboard goes here');
 });
 module.exports = router;
