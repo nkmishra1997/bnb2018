b5bfd56e657557586ff8ebb4cb74bf2dc12f8b29
Nikhil Mishra <nkmishra1997@gmail.com>
2017-07-11 00:41:09 +0530

    test

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '242522152904341', // your App ID
-        'clientSecret'  : '63aa33af249c83a91d6d706eec778db3', // your App Secret
+        'clientID'      : 'APP ID', // your App ID
+        'clientSecret'  : 'APP SECRET', // your App Secret
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
         'profileFields'   : ['id', 'displayName', 'name', 'gender' , 'email', 'photos']
     }
