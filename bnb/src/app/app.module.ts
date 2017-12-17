ebda4b589510a9e916e1373739727f97cef384b3
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2017-12-17 22:58:07 +0530

    buy/sell/short/cover forms added

--- bnb/src/app/app.module.ts
+++ bnb/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {RouterModule, Routes} from '@angular/router';
 
@@ -49,6 +49,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
--- bnb/src/app/services/company.service.ts
+++ bnb/src/app/services/company.service.ts
@@ -8,19 +8,39 @@ export class CompanyService {
   constructor(private http : Http) { }
 
   fetchCompany(id){
-    let headers = new Headers();
+    let headers = new Headers()
   //  let params = new URLSearchParams();
   //  params.set('id',id.toString())
-    headers.append('Content-Type','application/json');
+    headers.append('Content-Type','application/json')
     return this.http.get('/companydetail/' + id,{ headers : headers })
-    .map(res => res.json());
+    .map(res => res.json())
   }
-/*
-  fetchCustomer(){
-    let headers = new Headers();
-    headers.append('Content-Type','application/json');
-    return this.http.get('customerdetail',{headers : headers})
-    .map(res => res.json());
+
+  buy(id, form:Object){
+    let headers = new Headers()
+    headers.append('Content-Type','application/json')
+    return this.http.post('/buy/'+ id, JSON.stringify(form), {headers:headers})
+    .map(res => res.json())
   }
-*/
+
+  sell(id, form:Object){
+    let headers = new Headers()
+    headers.append('Content-Type','application/json')
+    return this.http.post('/sell/'+ id, JSON.stringify(form), {headers:headers})
+    .map(res => res.json())
+  }
+
+  short(id, form:Object){
+    let headers = new Headers()
+    headers.append('Content-Type','application/json')
+    return this.http.post('/short/'+ id, JSON.stringify(form), {headers:headers})
+    .map(res => res.json())
+  }
+
+  cover(id, form:Object){
+    let headers = new Headers()
+    headers.append('Content-Type','application/json')
+    return this.http.post('/cover/'+ id, JSON.stringify(form), {headers:headers})
+    .map(res => res.json())
+  }
 }
