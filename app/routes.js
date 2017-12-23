9ef94c868eb539ebd60a5a707888d58227626f66
Mohit.Gupta <mohitguptamt123@gmail.com>
2017-12-24 00:33:57 +0530

    bottom nav done

--- app/routes.js
+++ app/routes.js
@@ -91,7 +91,7 @@ app.route('/newslist')
 app.route('/customerdetail')
     .get(isLoggedIn, controller.customerDetail);
 
-app.route('/leaderboard')
+app.route('/scoreboard')
     .get(isLoggedIn, controller.customerList);
 
 app.route('/buy/:id')
