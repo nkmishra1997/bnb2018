1ea29d9c0f0256710e76b6857cd41d634562f072
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-01-06 23:26:47 +0530

    navbar code refactored

--- bnb/src/app/app.component.ts
+++ bnb/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ["./app.component.css",
+              "./components/css/bootstrap.min.css",
+              "./components/css/font-awesome.min.css",
+              "./components/css/style.css",]
 })
 export class AppComponent {
   title = 'BULLS  N  BEARS';
