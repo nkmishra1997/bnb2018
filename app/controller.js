b9a7a54e3642667a3124f9558c9cfcf1af39ef01
vegeta1509 <vegeta1509@users.noreply.github.com>
2018-02-07 23:27:13 +0530

    news time added

--- app/controller.js
+++ app/controller.js
@@ -41,7 +41,6 @@ exports.cryptoList = function(req, res){
     //var accountBal = {'accountBalance' : req.user.accountBalance}
     var cryptolist = []
     companies.forEach((element)=>{
-      console.log(element.annualGrowthRate);
       if(element.isCrypto==true){
       var crypto = {
         id: element._id,
