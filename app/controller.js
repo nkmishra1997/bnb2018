119355625a3d0489ce19cd233e4017371c147e7b
vegeta1509 <vegeta1509@users.noreply.github.com>
2018-02-07 21:58:00 +0530

    testing

--- app/controller.js
+++ app/controller.js
@@ -41,6 +41,7 @@ exports.cryptoList = function(req, res){
     //var accountBal = {'accountBalance' : req.user.accountBalance}
     var cryptolist = []
     companies.forEach((element)=>{
+      console.log(element);
       if(element.isCrypto==true){
       var crypto = {
         id: element._id,
--- bnb/src/app/components/market/market.component.ts
+++ bnb/src/app/components/market/market.component.ts
@@ -44,7 +44,7 @@ export class MarketComponent implements OnInit {
       this.subscription = Observable.timer(0, 10000)
       .subscribe(() => {
         this.marketService.fetchCompanies().subscribe(Companies => {
-          this.companies = Companies; console.log("company fetched");
+          this.companies = Companies; console.log(Companies);
         },
         err => {
           console.log(err);
