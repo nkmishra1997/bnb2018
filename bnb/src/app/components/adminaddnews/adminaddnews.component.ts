501ef553bec38cc68637024c658aa86fcd064858
Mohit.Gupta <mohitguptamt123@gmail.com>
2018-02-08 17:07:36 +0530

    addNews

--- bnb/src/app/components/adminaddnews/adminaddnews.component.ts
+++ bnb/src/app/components/adminaddnews/adminaddnews.component.ts
@@ -23,6 +23,35 @@ export class AdminaddnewsComponent implements OnInit {
   impact4: String = '';
   company5: String = '';
   impact5: String = '';
+  company6: String = '';
+  impact6: String = '';
+  company7: String = '';
+  impact7: String = '';
+  company8: String = '';
+  impact8: String = '';
+  company9: String = '';
+  impact9: String = '';
+  company10: String = '';
+  impact10: String = '';
+  company11: String = '';
+  impact11: String = '';
+
+  company12: String = '';
+  impact12: String = '';
+  company13: String = '';
+  impact13: String = '';
+  company14: String = '';
+  impact14: String = '';
+  company15: String = '';
+  impact15: String = '';
+  company16: String = '';
+  impact16: String = '';
+  company17: String = '';
+  impact17: String = '';
+  company18: String = '';
+  impact18: String = '';
+  company19: String = '';
+  impact19: String = '';
 
   constructor(
     private adminService: AdminService,
@@ -67,6 +96,90 @@ export class AdminaddnewsComponent implements OnInit {
       news.newsImpact.push({
         company : { _id: this.company5 },
         impact : this.impact5
+      });
+    }
+    if(this.company6!=''){
+      news.newsImpact.push({
+        company : { _id: this.company6 },
+        impact : this.impact6
+      });
+    }
+    if(this.company7!=''){
+      news.newsImpact.push({
+        company : { _id: this.company7 },
+        impact : this.impact7
+      });
+    }
+    if(this.company8!=''){
+      news.newsImpact.push({
+        company : { _id: this.company8 },
+        impact : this.impact8
+      });
+    }
+    if(this.company9!=''){
+      news.newsImpact.push({
+        company : { _id: this.company9},
+        impact : this.impact9
+      });
+    }
+    if(this.company10!=''){
+      news.newsImpact.push({
+        company : { _id: this.company10 },
+        impact : this.impact10
+      });
+    }
+    if(this.company11!=''){
+      news.newsImpact.push({
+        company : { _id: this.company11},
+        impact : this.impact11
+      });
+    }
+    if(this.company12!=''){
+      news.newsImpact.push({
+        company : { _id: this.company12 },
+        impact : this.impact12
+      });
+    }
+    if(this.company13!=''){
+      news.newsImpact.push({
+        company : { _id: this.company13 },
+        impact : this.impact13
+      });
+    }
+    if(this.company14!=''){
+      news.newsImpact.push({
+        company : { _id: this.company14 },
+        impact : this.impact14
+      });
+    }
+    if(this.company15!=''){
+      news.newsImpact.push({
+        company : { _id: this.company15 },
+        impact : this.impact15
+      });
+    }
+    if(this.company16!=''){
+      news.newsImpact.push({
+        company : { _id: this.company16 },
+        impact : this.impact16
+      });
+    }
+    if(this.company17!=''){
+      news.newsImpact.push({
+        company : { _id: this.company17 },
+        impact : this.impact17
+      });
+    }
+    if(this.company18!=''){
+      news.newsImpact.push({
+        company : { _id: this.company18 },
+        impact : this.impact18
+      });
+    }
+    if(this.company19!=''){
+      news.newsImpact.push({
+        company : { _id: this.company19 },
+        impact : this.impact19
       });
     }
   
